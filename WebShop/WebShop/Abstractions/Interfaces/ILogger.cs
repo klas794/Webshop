540edc30920277b340c2f61c990c3202c1fa5877
Klas <klas793@gmail.com>
2016-09-20 19:27:48 +0200

    Added log class, interface and test file

---
+++ WebShop/WebShop/Abstractions/Interfaces/ILogger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebShop.Abstractions.Interfaces
+{
+    interface ILogger
+    {
+
+        void WriteLogMessage(string message);
+
+        string[] ReadLogContent();
+
+        void Clear();
+    }
+}

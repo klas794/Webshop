292144beb551f1b7375a141ad3d70b0db7237cd8
Tanja Svärd <tanja_jazz@hotmail.com>
2016-09-20 15:17:57 +0200

    nya interface

---
+++ WebShop/WebShop/Abstractions/IBaseProducts.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebShop.Abstractions
+{
+    interface IBaseProducts
+    {
+        void ReadItem();
+        void SaveItem();
+        void UpdateItem();
+        void DeleteItem(); 
+    }
+}

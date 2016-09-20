387a6566bcf1a6a9393b1161291af1bd666a8a0e
CanSlivo <can.slivo@gmail.com>
2016-09-20 19:05:51 +0200

    Skapat en produkt klass

---
+++ WebShop/WebShop/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebShop
+{
+    public class Product
+    {
+        //Egenskaper som vår klass/product har
+
+        public int Id { get; set; }
+        public int Price { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+
+        public Product()
+        {
+            
+        } 
+    }
+}

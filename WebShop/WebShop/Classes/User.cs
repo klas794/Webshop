0f8d255e92a0ba1b21e4bd87157026169391e16f
MadokaFujimaki <madoka.fujimaki@me.com>
2016-09-21 13:05:55 +0200

    add product class and user class

---
+++ WebShop/WebShop/Classes/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebShop.Classes
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public int Password { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+       
+    }
+}

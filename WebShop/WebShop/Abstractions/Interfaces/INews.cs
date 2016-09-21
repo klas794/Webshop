1aca14ca563c5e265eced247f6acaf4ed52584d0
annasandberg <Anna.Kallman@yh.nackademin.se>
2016-09-21 21:29:16 +0200

    Vy för att visa nyheter

--- WebShop/WebShop/Abstractions/Interfaces/INews.cs
+++ WebShop/WebShop/Abstractions/Interfaces/INews.cs
@@ -3,11 +3,11 @@ namespace WebShop.Abstractions
 {
     interface INews
     {
-        //List<Product> Products { get; set; } //För att returnera en lista med produkter
-        string NewsDescription { get; set; } //om vi vill ha en beskrivande text på nyhetssidan.
-        string Gender { get; set; }
-        string ProductType { get; set; }
-        bool DisplayImageSlideshow { get; set; } //Så att man tex kan dölja bildspelet överst i tex en mobilversion av sidan.
+        //List<Products> NewProducts(); //För att returnera en lista med produkter
+        string NewsDescription(); //om vi vill ha en beskrivande text på nyhetssidan.
+        //string Gender { get; set; } //för att visa nyheter för dam eller herr.
+        //string ProductType { get; set; }//för att kunna sortera på produkttyp
+        bool DisplayImageSlideshow(); //Så att man tex kan dölja bildspelet överst i tex en mobilversion av sidan.
 
     }
 }

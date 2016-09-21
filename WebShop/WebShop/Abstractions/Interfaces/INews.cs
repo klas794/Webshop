919ff6019551063ea53d37c8e83853f3c3e50546
annasandberg <Anna.Kallman@yh.nackademin.se>
2016-09-21 17:50:57 +0200

    Revert "News class and updated interface. Created a NewsPage but no content."
    This reverts commit 33b404d774dae5c01605d970bf91f8416e16d41f.

--- WebShop/WebShop/Abstractions/Interfaces/INews.cs
+++ WebShop/WebShop/Abstractions/Interfaces/INews.cs
@@ -1,18 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WebShop.Abstractions.Interfaces
+﻿
+namespace WebShop.Abstractions
 {
     interface INews
     {
-        //List<Products> NewProducts(); //För att returnera en lista med produkter
-        string NewsDescription(); //om vi vill ha en beskrivande text på nyhetssidan.
-        //string Gender { get; set; } //för att visa nyheter för dam eller herr.
-        //string ProductType { get; set; }//för att kunna sortera på produkttyp
-        bool DisplayImageSlideshow(); //Så att man tex kan dölja bildspelet överst i tex en mobilversion av sidan.
+        //List<Product> Products { get; set; } //För att returnera en lista med produkter
+        string NewsDescription { get; set; } //om vi vill ha en beskrivande text på nyhetssidan.
+        string Gender { get; set; }
+        string ProductType { get; set; }
+        bool DisplayImageSlideshow { get; set; } //Så att man tex kan dölja bildspelet överst i tex en mobilversion av sidan.
 
     }
 }

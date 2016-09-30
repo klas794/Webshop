14ddb42e5b9b7e31e1384b48ebf6cbbc91adaf4a
Klas <klas793@gmail.com>
2016-09-30 15:03:00 +0200

    some decorative updates to the front page

---
+++ WebShop/WebShop/Classes/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebShop.Classes
+{
+    public class Constants
+    {
+        public const string PathProductsImages = "~/Images/ProductImages/";
+    }
+}
